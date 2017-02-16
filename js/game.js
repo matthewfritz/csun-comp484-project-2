276d994b56c8d3f03c16766e01cd67d5f4a24952
Matthew Fritz <matthew.fritz@csun.edu>
2017-02-16 12:58:15 -0800

    Finished the simple implementation of the game

--- js/game.js
+++ js/game.js
@@ -11,14 +11,14 @@
 //
 // =============================
 
-// maximum number of guesses for the words
-const MAX_GUESSES = 7;
+// maximum number of misses for the words
+const MAX_MISSES = 7;
 
-// number of guesses that have been attempted
-var num_guesses = 0;
+// number of misses that have taken place
+var num_misses = 0;
 
-// true if the game has been won, false otherwise
-var won = false;
+// true if the game has ended, false otherwise
+var ended = false;
 
 // set of available words for guessing
 var words = [
@@ -115,8 +115,8 @@ var image_seven = `
 
 // starts the game, selects a random word, and applies it
 function startGame() {
-	num_guesses = 0;
-	won = false;
+	num_misses = 0;
+	ended = false;
 
 	// grab the reference to the image element and guessed element
 	image_element = document.getElementById('game-image');
@@ -131,7 +131,7 @@ function startGame() {
 
 	// select a new random word
 	let word_index = Math.floor(Math.random() * words.length);
-	current_word = words[word_index];
+	current_word = words[word_index].toUpperCase();
 
 	// split the word into its character array
 	current_word_characters = current_word.split("");
@@ -140,55 +140,75 @@ function startGame() {
 	guessed = current_word_characters.slice();
 	guessed.fill('_');
 
+	// display the word text
+	displayWord();
+
 	console.info("Game started");
 }
 
 function displayImage() {
 	let image = "";
 
-	if(num_guesses == 1) {
+	if(num_misses == 1) {
 		image = image_one;
 	}
-	else if(num_guesses == 2) {
+	else if(num_misses == 2) {
 		image = image_two;
 	}
-	else if(num_guesses == 3) {
+	else if(num_misses == 3) {
 		image = image_three;
 	}
-	else if(num_guesses == 4) {
+	else if(num_misses == 4) {
 		image = image_four;
 	}
-	else if(num_guesses == 5) {
+	else if(num_misses == 5) {
 		image = image_five;
 	}
-	else if(num_guesses == 6) {
+	else if(num_misses == 6) {
 		image = image_six;
 	}
-	else if(num_guesses == MAX_GUESSES) {
+	else if(num_misses == MAX_MISSES) {
 		image = image_seven;
 	}
 
 	// update the image element on the document
 	image_element.innerHTML = "<pre>" + image + "</pre>";
+}
 
+function displayWord() {
 	// update the guesses on the screen
-	guessed_element.innerHTML = "<pre>Word: " + guessed.split(" ") + "</pre>";
+	guessed_element.innerHTML = "<pre>Word: " + guessed.join(" ") + "</pre><br />Misses: " + num_misses + "/" + MAX_MISSES;
 }
 
 // checks whether the game has been won; returns true if the game has
 // been won, false otherwise
 function checkGuess(letter) {
-	for(let i = 0; i < current_word_characters.length; i++) {
-		if(current_word_characters[i] == letter) {
-			// set the corresponding letter on the guessed characters
-			guessed[i] = letter;
-		}
-	}
+	if(num_misses < MAX_MISSES) {
+		let replacements = 0;
+		for(let i = 0; i < current_word_characters.length; i++) {
+			if(current_word_characters[i] == letter) {
+				// set the corresponding letter on the guessed characters
+				guessed[i] = letter;
 
-	// do the words match?
-	if(guessed.join('') == current_word) {
-		// the word has been guessed completely
-		return true;
+				replacements++;
+			}
+		}
+
+		// only update the game image and max guesses if there were
+		// no replacements
+		if(replacements == 0) {
+			// increment the number of guesses
+			num_misses++;
+
+			// display the new image
+			displayImage();
+		}
+
+		// do the words match?
+		if(guessed.join('') == current_word) {
+			// the word has been guessed completely
+			return true;
+		}
 	}
 
 	return false;
@@ -196,28 +216,23 @@ function checkGuess(letter) {
 
 // make the guess
 function makeGuess(letter) {
-	if(!won) {
-		if(checkGuess()) {
+	if(!ended) {
+		if(checkGuess(letter)) {
 			// game won, so display a message on the screen above the image
-			image_element.innerHTML += "<strong>YOU WON! Select Start New Game to play again</strong>";
-			won = true;
+			image_element.innerHTML += "<br /><strong>YOU WON! Select Start New Game to play again</strong>";
+			ended = true;
 		}
 		else
 		{
-			// only execute if the player has not yet lost
-			if(num_guesses < MAX_GUESSES) {
-				// increment the number of guesses
-				num_guesses++;
-
-				// display the new image
-				displayImage();
-
-				// was this a losing guess?
-				if(num_guesses == MAX_GUESSES) {
-					// player lost
-					image_element.innerHTML += "<strong>You lost. The word was '" + current_word + "'. Select Start New Game to play again</strong>";
-				}
+			// has the player lost?
+			if(num_misses == MAX_MISSES) {
+				// player lost
+				image_element.innerHTML += "<br /><strong>You lost. The word was '" + current_word + "'. Select Start New Game to play again</strong>";
+				ended = true;
 			}
 		}
+
+		// update the word display
+		displayWord();
 	}
 }
